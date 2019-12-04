ca092edf220fee0fbf221cdb330a100acba685cc
E187420S <alexandre.rouchouze@etu.univ-nantes.fr>
2019-12-04 20:05:18 +0100

    modification extension et renommage

---
+++ src/InfluxDB/TableReleves.ts
@@ -0,0 +1,19 @@
+const Influx = require('influx');
+const influx = new Influx.InfluxDB({
+database: 'SunShare',
+  schema: [
+    {
+      measurement: 'EnergyRecord',
+      fields: {
+        production: Influx.FieldType.INTEGER,
+        consumption: Influx.FieldType.INTEGER,
+        surplus: Influx.FieldType.INTEGER
+      },
+      tags: [
+          'created_by'
+      ]
+    }
+]
+})
+
+export default influx;
