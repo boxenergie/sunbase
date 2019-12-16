0c33cf00f2abd6043cf55b1cabb00be1762cb2f2
Teddy EVRARD <teddy.evrard@etu.univ-nantes.fr>
2019-12-16 14:06:53 +0100

    Add MongoDB Support

---
+++ src/models/User.ts
@@ -0,0 +1,16 @@
+import MongoClient from '../db/mongodb';
+import { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+	username: string;
+	password: string;
+	role: string;
+}
+
+const userSchema = new Schema<IUser>({
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+	role: { type: String, required: true, default: 'user' }
+}); 
+
+export default MongoClient.model<IUser>('User', userSchema);
