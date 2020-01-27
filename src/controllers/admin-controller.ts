6000102f2f0ef5f6897849430ed414cda2ab8d48
Teddy EVRARD <teddy.evrard@etu.univ-nantes.fr>
2020-01-27 12:39:22 +0100

    Fix admin page
    - Display success and error message
    - Use query parameters instead of body parameters
    - Delete user

--- src/controllers/admin-controller.ts
+++ src/controllers/admin-controller.ts
@@ -4,14 +4,16 @@ import { NextFunction, Response, Request } from 'express';
 import User from '../models/User';
 
 export async function renderAdminPage(req: Request, res: Response, next: NextFunction) {
-    if (req.body.deleted) {
-        deleteUser(req, res, next);
-        return;
+    if (req.query.deleted) {
+        return deleteUser(req, res, next);
     }
+
 	try {
         const users = await User.find().limit(10).exec();
         res.send(Sqrl.renderFile('./views/adminpage.squirrelly', {
-            users
+            users: users,
+            errorMsg: req.flash('errorMsg'),
+            successMsg: req.flash('successMsg'),
         }));
 	} catch (err) {
 		console.error(err);
@@ -22,14 +24,14 @@ export async function renderAdminPage(req: Request, res: Response, next: NextFun
 export async function deleteUser(req: Request, res: Response, next: NextFunction) {
     try {
         let errorMsg = null;
-        const deletedUserId = req.body.deleted;
+        const deletedUserId = req.query.deleted;
 
         if (!deletedUserId) {
             errorMsg = 'One or more fields were not provided.';
         }        
 
         try {
-            await User.deleteOne({id: deletedUserId});
+            await User.deleteOne({_id: deletedUserId});
             req.flash('successMsg', 'User deleted.');
             return res.redirect('/admin');
         } catch (e) {
