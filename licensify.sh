8b909b7d1c86a522dd68ca0f38354275c664606f
Fabien HERVÉ <fabien.herve1@etu.univ-nantes.fr>
2020-01-30 11:35:00 +0100

    Add license headers

---
+++ licensify.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# Copyright 2015 Remi Even
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+old_ifs=$IFS
+
+# You can modify the following variables
+offset=0
+
+author="Sunshare"
+year="2019"
+project_name="Sunbase"
+
+ext="ts"
+
+comment_start='/*'
+comment_middle=' *'
+comment_end=' */'
+
+license='{{file_name}}
+Copyright (C) {{author}} {{year}}
+This file is part of {{project_name}}.
+{{project_name}} is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as 
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+{{project_name}} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with {{project_name}}. If not, see <http://www.gnu.org/licenses/>.'
+
+# End of modifiable variables
+
+cwd=$(dirname "$0")
+
+verbose=false
+
+function generate_header_content {
+file_name=$1
+    echo -e "$license" \
+    | sed -e "s/{{author}}/$author/g" \
+    | sed -e "s/{{year}}/$year/g" \
+    | sed -e "s/{{project_name}}/$project_name/g" \
+    | sed -e "s/{{file_name}}/$file_name/g" \
+    | sed -e "s/\(.*\)/$comment_middle \1/" \
+    | sed -e "s/[ \t]*$//" \
+    >> .licensifytmp
+}
+
+function add_header {
+    goal=$1
+    if [[ ! -f $goal ]]; then
+        echo "Warning : Can not find file \"$goal\""
+        return
+    fi
+    file_name=$(basename $goal)
+    i=0
+    touch .licensifytmp
+    while IFS='' read -r line || [[ -n $line ]]; do
+        if (( $i == $offset )); then
+            echo "$comment_start" >> .licensifytmp
+            generate_header_content $file_name
+            echo -e "$comment_end\n" >> .licensifytmp
+            if [[ $verbose = true ]]; then
+                echo "Info : Added header to $(basename $goal)"
+            fi
+        fi
+        (( i++ ))
+        echo "$line" >> .licensifytmp
+    done < $goal
+    mv .licensifytmp $goal
+}
+
+if [[ $1 = '-v' ]]; then
+    verbose=true
+    shift
+fi
+
+if (( $# == 0 )); then
+    goals=$(find $cwd/src -name \*.$ext -type f -print | xargs grep -L "Copyright (C)")
+else
+    goals=$*
+fi
+
+for goal in $goals; do
+    add_header $goal
+done
+
+IFS=$old_ifs
